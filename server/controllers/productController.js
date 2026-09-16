3e7c72759bf4a4a72d4bef1b0b1835ea8de2b90e
Oussama Ezzine <getintooussamaezzine@gmail.com>
2026-09-16 17:54:41 +0100

    1st commit

---
+++ server/controllers/productController.js
@@ -0,0 +1,103 @@
+const Product = require("../models/Product");
+
+// CREATE
+const createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message
+    });
+  }
+};
+
+// READ ALL
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
+
+// READ ONE
+const getProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found"
+      });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message
+    });
+  }
+};
+
+// UPDATE
+const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true
+      }
+    );
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found"
+      });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message
+    });
+  }
+};
+
+// DELETE
+const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(
+      req.params.id
+    );
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found"
+      });
+    }
+
+    res.status(200).json({
+      message: "Product deleted successfully"
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message
+    });
+  }
+};
+
+module.exports = {
+  createProduct,
+  getProducts,
+  getProduct,
+  updateProduct,
+  deleteProduct
+};
